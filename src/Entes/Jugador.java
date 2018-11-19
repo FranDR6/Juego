08464a862ed5f2a97230d66dcc80c34ca6e7ea80
Coral <Coral@192.168.1.91>
2018-11-19 13:48:34 +0100

    animacion cuado no se esta moviendo

--- src/Entes/Jugador.java
+++ src/Entes/Jugador.java
@@ -14,6 +14,8 @@ public class Jugador {
 
 	private int estadoAnimacion;
 
+	private boolean enMovimiento = false;
+
 	private int direccion;
 
 	private HojaSprites hojaPersonaje;
@@ -43,54 +45,98 @@ public class Jugador {
 	}
 
 	public void actualizar() {
+
 		if (GestorControles.teclado.isArriba() && !GestorControles.teclado.isIzquierda()
 				&& !GestorControles.teclado.isDerecha()) {
 			direccion = 0;
 			animar(direccion);
 			posicionY -= Constantes.velocidadPersonajeAndando;
 		}
+
 		if (GestorControles.teclado.isAbajo() && !GestorControles.teclado.isIzquierda()
 				&& !GestorControles.teclado.isDerecha()) {
 			direccion = 1;
 			animar(direccion);
 			posicionY += Constantes.velocidadPersonajeAndando;
 		}
+
 		if (GestorControles.teclado.isIzquierda() && !GestorControles.teclado.isAbajo()
 				&& !GestorControles.teclado.isArriba()) {
 			direccion = 2;
 			animar(direccion);
 			posicionX -= Constantes.velocidadPersonajeAndando;
 		}
+
 		if (GestorControles.teclado.isDerecha() && !GestorControles.teclado.isAbajo()
 				&& !GestorControles.teclado.isArriba()) {
 			direccion = 3;
 			animar(direccion);
 			posicionX += Constantes.velocidadPersonajeAndando;
 		}
+
 		if (GestorControles.teclado.isAbajo() && GestorControles.teclado.isIzquierda()) {
 			direccion = 4;
 			animar(direccion);
 			posicionY += Constantes.velocidadPersonajeAndando;
 			posicionX -= Constantes.velocidadPersonajeAndando;
 		}
+
 		if (GestorControles.teclado.isAbajo() && GestorControles.teclado.isDerecha()) {
 			direccion = 5;
 			animar(direccion);
 			posicionY += Constantes.velocidadPersonajeAndando;
 			posicionX += Constantes.velocidadPersonajeAndando;
 		}
+
 		if (GestorControles.teclado.isArriba() && GestorControles.teclado.isIzquierda()) {
 			direccion = 6;
 			animar(direccion);
 			posicionY -= Constantes.velocidadPersonajeAndando;
 			posicionX -= Constantes.velocidadPersonajeAndando;
 		}
+
 		if (GestorControles.teclado.isArriba() && GestorControles.teclado.isDerecha()) {
 			direccion = 7;
 			animar(direccion);
 			posicionY -= Constantes.velocidadPersonajeAndando;
 			posicionX += Constantes.velocidadPersonajeAndando;
+		}
 
+		if (GestorControles.teclado.isArriba() || GestorControles.teclado.isAbajo()
+				|| GestorControles.teclado.isIzquierda() || GestorControles.teclado.isDerecha()) {
+			enMovimiento = true;
+
+		} else {
+			enMovimiento = false;
+		}
+
+		if (!enMovimiento) {
+			switch (direccion) {
+			case 0:
+				imagenActual = hojaPersonaje.obtenerSprite(0, 1).getImagen();
+				break;
+			case 1:
+				imagenActual = hojaPersonaje.obtenerSprite(0, 0).getImagen();
+				break;
+			case 2:
+				imagenActual = hojaPersonaje.obtenerSprite(0, 2).getImagen();
+				break;
+			case 3:
+				imagenActual = hojaPersonaje.obtenerSprite(0, 3).getImagen();
+				break;
+			case 4:
+				imagenActual = hojaPersonaje.obtenerSprite(0, 4).getImagen();
+				break;
+			case 5:
+				imagenActual = hojaPersonaje.obtenerSprite(0, 5).getImagen();
+				break;
+			case 6:
+				imagenActual = hojaPersonaje.obtenerSprite(0, 6).getImagen();
+				break;
+			case 7:
+				imagenActual = hojaPersonaje.obtenerSprite(0, 7).getImagen();
+				break;
+			}
 		}
 	}
 
