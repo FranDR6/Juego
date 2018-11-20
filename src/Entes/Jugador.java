5f3c040a609e3550303e79e5a421fca8c9db0d2d
Coral <Coral@192.168.1.91>
2018-11-20 08:56:56 +0100

    cambio en los controles de movimiento

--- src/Entes/Jugador.java
+++ src/Entes/Jugador.java
@@ -26,7 +26,7 @@ public class Jugador {
 		this.posicionX = posicionX;
 		this.posicionY = posicionY;
 
-		estadoAnimacion = 0;
+		estadoAnimacion = 1;
 
 		direccion = 0;
 
@@ -40,67 +40,65 @@ public class Jugador {
 		int centroY = Constantes.altoPantalla / 2 - 32 / 2;
 
 		g.setColor(Color.RED);
-		g.drawRect(centroX + 10, centroY + 4, 64 - 20, 64 - 8);
+//		g.drawRect(centroX + 10, centroY + 4, 64 - 20, 64 - 8);
 		g.drawImage(imagenActual, centroX, centroY, null);
 	}
 
 	public void actualizar() {
 
 		if (GestorControles.teclado.isArriba() && !GestorControles.teclado.isIzquierda()
-				&& !GestorControles.teclado.isDerecha()) {
+				&& !GestorControles.teclado.isDerecha() && !GestorControles.teclado.isAbajo()) {
 			direccion = 0;
-			animar(direccion);
 			posicionY -= Constantes.velocidadPersonajeAndando;
 		}
 
-		if (GestorControles.teclado.isAbajo() && !GestorControles.teclado.isIzquierda()
-				&& !GestorControles.teclado.isDerecha()) {
+		if (!GestorControles.teclado.isArriba() && !GestorControles.teclado.isIzquierda()
+				&& !GestorControles.teclado.isDerecha() && GestorControles.teclado.isAbajo()) {
 			direccion = 1;
-			animar(direccion);
 			posicionY += Constantes.velocidadPersonajeAndando;
 		}
 
-		if (GestorControles.teclado.isIzquierda() && !GestorControles.teclado.isAbajo()
-				&& !GestorControles.teclado.isArriba()) {
+		if (!GestorControles.teclado.isArriba() && GestorControles.teclado.isIzquierda()
+				&& !GestorControles.teclado.isDerecha() && !GestorControles.teclado.isAbajo()) {
 			direccion = 2;
-			animar(direccion);
 			posicionX -= Constantes.velocidadPersonajeAndando;
 		}
 
-		if (GestorControles.teclado.isDerecha() && !GestorControles.teclado.isAbajo()
-				&& !GestorControles.teclado.isArriba()) {
+		if (!GestorControles.teclado.isArriba() && !GestorControles.teclado.isIzquierda()
+				&& GestorControles.teclado.isDerecha() && !GestorControles.teclado.isAbajo()) {
 			direccion = 3;
-			animar(direccion);
 			posicionX += Constantes.velocidadPersonajeAndando;
 		}
 
-		if (GestorControles.teclado.isAbajo() && GestorControles.teclado.isIzquierda()) {
+		if (!GestorControles.teclado.isArriba() && GestorControles.teclado.isIzquierda()
+				&& !GestorControles.teclado.isDerecha() && GestorControles.teclado.isAbajo()) {
 			direccion = 4;
-			animar(direccion);
 			posicionY += Constantes.velocidadPersonajeAndando;
 			posicionX -= Constantes.velocidadPersonajeAndando;
 		}
 
-		if (GestorControles.teclado.isAbajo() && GestorControles.teclado.isDerecha()) {
+		if (!GestorControles.teclado.isArriba() && !GestorControles.teclado.isIzquierda()
+				&& GestorControles.teclado.isDerecha() && GestorControles.teclado.isAbajo()) {
 			direccion = 5;
-			animar(direccion);
 			posicionY += Constantes.velocidadPersonajeAndando;
 			posicionX += Constantes.velocidadPersonajeAndando;
 		}
 
-		if (GestorControles.teclado.isArriba() && GestorControles.teclado.isIzquierda()) {
+		if (GestorControles.teclado.isArriba() && GestorControles.teclado.isIzquierda()
+				&& !GestorControles.teclado.isDerecha() && !GestorControles.teclado.isAbajo()) {
 			direccion = 6;
-			animar(direccion);
 			posicionY -= Constantes.velocidadPersonajeAndando;
 			posicionX -= Constantes.velocidadPersonajeAndando;
 		}
 
-		if (GestorControles.teclado.isArriba() && GestorControles.teclado.isDerecha()) {
+		if (GestorControles.teclado.isArriba() && !GestorControles.teclado.isIzquierda()
+				&& GestorControles.teclado.isDerecha() && !GestorControles.teclado.isAbajo()) {
 			direccion = 7;
-			animar(direccion);
 			posicionY -= Constantes.velocidadPersonajeAndando;
 			posicionX += Constantes.velocidadPersonajeAndando;
 		}
+
+		animar(direccion);
 
 		if (GestorControles.teclado.isArriba() || GestorControles.teclado.isAbajo()
 				|| GestorControles.teclado.isIzquierda() || GestorControles.teclado.isDerecha()) {
@@ -147,55 +145,32 @@ public class Jugador {
 		if (Constantes.APS % frecuenciaAnimacion == 0) {
 			estadoAnimacion++;
 			if (estadoAnimacion >= limiteEstados) {
-				estadoAnimacion = 0;
+				estadoAnimacion = 1;
 			}
+
 			switch (direccion) {
 			case 0:
-				if (estadoAnimacion == 0) {
-					estadoAnimacion++;
-				}
 				imagenActual = hojaPersonaje.obtenerSprite(estadoAnimacion, 1).getImagen();
 				break;
 			case 1:
-				if (estadoAnimacion == 0) {
-					estadoAnimacion++;
-				}
 				imagenActual = hojaPersonaje.obtenerSprite(estadoAnimacion, 0).getImagen();
 				break;
 			case 2:
-				if (estadoAnimacion == 0) {
-					estadoAnimacion++;
-				}
 				imagenActual = hojaPersonaje.obtenerSprite(estadoAnimacion, 2).getImagen();
 				break;
 			case 3:
-				if (estadoAnimacion == 0) {
-					estadoAnimacion++;
-				}
 				imagenActual = hojaPersonaje.obtenerSprite(estadoAnimacion, 3).getImagen();
 				break;
 			case 4:
-				if (estadoAnimacion == 0) {
-					estadoAnimacion++;
-				}
 				imagenActual = hojaPersonaje.obtenerSprite(estadoAnimacion, 4).getImagen();
 				break;
 			case 5:
-				if (estadoAnimacion == 0) {
-					estadoAnimacion++;
-				}
 				imagenActual = hojaPersonaje.obtenerSprite(estadoAnimacion, 5).getImagen();
 				break;
 			case 6:
-				if (estadoAnimacion == 0) {
-					estadoAnimacion++;
-				}
 				imagenActual = hojaPersonaje.obtenerSprite(estadoAnimacion, 6).getImagen();
 				break;
 			case 7:
-				if (estadoAnimacion == 0) {
-					estadoAnimacion++;
-				}
 				imagenActual = hojaPersonaje.obtenerSprite(estadoAnimacion, 7).getImagen();
 				break;
 			}
